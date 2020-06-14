4af5da99f79ef82393bb8c9b1878bedf42a3f38c
Dominic <Dominic@CONTROLLER>
2020-06-14 21:15:41 +0100

    add active record

--- Src/DesktopClient/BasicCRUDTool3.Data/Models/ActiveRecord.cs
+++ Src/DesktopClient/BasicCRUDTool3.Data/Models/ActiveRecord.cs
@@ -1,9 +1,43 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace BasicCRUDTool3.Data.Models
 {
     public abstract class ActiveRecord<TEntity, TKey> where TEntity : class, new()
     {
-        protected readonly ICRUDTestDBContextProvider CRUDTestDBProvider
+        #region Protected members
+        protected readonly ICRUDTestDBContextProvider CRUDTestDBProvider;
+        protected readonly CRUDTestDBContext Context;
+        protected TEntity Entity;
+        #endregion
+
+        #region Public Properties
+        public TKey Id { get; protected set; }
+        #endregion
+
+        protected ActiveRecord(ICRUDTestDBContextProvider cRUDTestDBProvider)
+        {
+            CRUDTestDBProvider = cRUDTestDBProvider;
+            Context = cRUDTestDBProvider.GetContext();
+        }
+
+        public virtual void Load(TKey id)
+        {
+            Entity = Context.Find<TEntity>(id);
+            if (Entity != null)
+            {
+                Id = id;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public virtual void New()
+        {
+            Entity = new TEntity();
+            Context.Add(Entity);
+        }
     }
 }
--- Tests/BasicCRUDTool3.Data.Tests/FunctionalTests/CustomerScenarioTests.cs
+++ Tests/BasicCRUDTool3.Data.Tests/FunctionalTests/CustomerScenarioTests.cs
@@ -9,22 +9,5 @@ namespace BasicCRUDTool3.Data.Tests.FunctionalTests
 {
     public class CustomerScenarioTests
     {
-        [TestMethod]
-        public void CanCreateCustomer()
-        {
-            using NpgsqlConnection conn = new NpgsqlConnection("Host=localhost;Database=CRUDTestDB;Username=postgres;Password=Password12!");
-            {
-                try
-                {
-                    IServiceCollection services = new ServiceCollection();
-                    services.AddDbContext
-                }
-                catch (NpgsqlException e)
-                {
-                    Console.WriteLine(e.ToString());
-                    Assert.Fail("Connection to test server has failed");
-                }
-            }
-        }
     }
 }
