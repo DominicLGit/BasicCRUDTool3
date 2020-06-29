aaf654e4ebcd857d868318bfab4dce9d5567d4df
DominicLGit <dominic.langsdale@gmail.com>
2020-06-29 18:49:27 +0100

    Added PlaylistTrackBE unit tests

--- Tests/BasicCRUDTool3.Business.Tests/UnitTests/PlaylistTrackBETests.cs
+++ Tests/BasicCRUDTool3.Business.Tests/UnitTests/PlaylistTrackBETests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using BasicCRUDTool3.Data.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,7 +7,64 @@ using System.Text;
 namespace BasicCRUDTool3.Business.Tests.UnitTests
 {
     [TestClass]
-    class PlaylistTrackBETests
+    public class PlaylistTrackBETests
     {
+        /// <summary>
+        /// Test for loading existing object from database with all attributes
+        /// Test for loading existing object from database with missing non-required attributes from relationships
+        /// </summary>
+        [TestMethod]
+        public void LoadValidIdTest()
+        {
+            ICRUDTestDBContextProvider cRUDTestDBContextProvider = new CRUDTestDBContextProvider(Guid.NewGuid().ToString());
+            var context = cRUDTestDBContextProvider.GetContext();
+            var playlistTrack = new PlaylistTrack
+            {
+                PlaylistId = 1,
+                TrackId = 1
+            };
+            var playlist = new Playlist
+            {
+                Name = "TestPlaylistName",
+                PlaylistId = 1
+            };
+            var track = new Track
+            {
+                TrackId = 1,
+                Name = "TestTrackName"
+            };
+            var playlistTrack2 = new PlaylistTrack
+            {
+                PlaylistId = 2,
+                TrackId = 2
+            };
+            var playlist2 = new Playlist
+            {
+                PlaylistId = 2
+            };
+            var track2 = new Track
+            {
+                TrackId = 2,
+                Name = "TestTrackName"
+            };
+            context.Add(playlistTrack);
+            context.Add(playlist);
+            context.Add(track);
+            context.Add(playlistTrack2);
+            context.Add(playlist2);
+            context.Add(track2);
+            context.SaveChanges();
+
+            PlaylistTrackBE playlistTrackBE = new PlaylistTrackBE(cRUDTestDBContextProvider);
+            playlistTrackBE.Load((1,1), 1, 1);
+            Assert.IsTrue(playlistTrackBE.Id == (1,1));
+            Assert.IsTrue(playlistTrackBE.PlaylistName == "TestPlaylistName");
+            Assert.IsTrue(playlistTrackBE.TrackName == "TestTrackName");
+
+            PlaylistTrackBE playlistTrackBE2 = new PlaylistTrackBE(cRUDTestDBContextProvider);
+            playlistTrackBE2.Load((2, 2), 2, 2);
+            Assert.IsTrue(playlistTrackBE2.Id == (2,2));
+            Assert.IsTrue(playlistTrackBE2.TrackName == "TestTrackName");
+        }
     }
 }
