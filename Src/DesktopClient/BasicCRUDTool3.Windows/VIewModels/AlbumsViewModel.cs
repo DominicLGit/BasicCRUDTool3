c1985c2765997b102a3c2d20cb738f45ded2ce3d
DominicLGit <dominic.langsdale@gmail.com>
2020-07-04 13:04:23 +0100

    Changed framework target to include netstandard2.1 to support blazor

---
+++ Src/DesktopClient/BasicCRUDTool3.Windows/VIewModels/AlbumsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCRUDTool3.Windows.VIewModels
+{
+    public class AlbumsViewModel : ViewModel
+    {
+    }
+}
---
+++ Src/DesktopClient/BasicCRUDTool3.Windows/VIewModels/IAlbumViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BasicCRUDTool3.Business;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCRUDTool3.Windows.VIewModels
+{
+    public interface IAlbumViewModel
+    {
+        public AlbumBE[] Albums { get; set; }
+    }
+}
